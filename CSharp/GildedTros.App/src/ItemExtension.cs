806f4b6541aa6b6371267eaf7d96602ae8765cd8
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-10-29 14:46:11 +0100

    Refactor Update method

--- CSharp/GildedTros.App/src/ItemExtension.cs
+++ CSharp/GildedTros.App/src/ItemExtension.cs
@@ -1,95 +1,98 @@
-﻿namespace GildedTros.App
+﻿using System.ComponentModel.Design;
+
+namespace GildedTros.App
 {
     internal static class ItemExtensions
     {
+        const int MinQuality = 0, MaxQuality = 50;
+
         internal static void Update(this Item item)
         {
-            if (item.Name != "Good Wine"
-                && item.Name != "Backstage passes for HAXX"
-                && item.Name != "Backstage passes for Re:factor")
+            // update quality
+            switch (item.Name)
             {
-                if (item.Name != "B-DAWG Keychain")
-                {
-                    if (item.Name == "Duplicate Code" ||
-                        item.Name == "Long Methods" ||
-                        item.Name == "Ugly Variable Names")
-                    {
-                        item.Quality = System.Math.Max(0, item.Quality - 2);
-                    }
-                    else
-                    {
-                        item.Quality = System.Math.Max(0, item.Quality - 1);
-                    }
-                }
+                case "Good Wine":
+                    UpdateQualityForGoodWine(ref item);
+                    break;
+                case "Backstage passes for HAXX":
+                case "Backstage passes for Re:factor":
+                    UpdateQualityForBackstagePasses(ref item);
+                    break;
+                case "Duplicate Code":
+                case "Long Methods":
+                case "Ugly Variable Names":
+                    UpdateQualityForSmellyItems(ref item);
+                    break;
+                case "B-DAWG Keychain":
+                    // legendary items always keep their quality
+                    break;
+                default:
+                    UpdateQualityForNormalItem(ref item);
+                    break;
+            }
+
+            // update "sell in"
+            if (item.Name != "B-DAWG Keychain")
+            {
+                item.SellIn -= 1;
+            }
+        }
+
+
+        static void UpdateQualityForGoodWine(ref Item item)
+        {
+            item.Quality = Clamp(item.Quality + Decay(item));
+        }
+
+        static void UpdateQualityForBackstagePasses(ref Item item)
+        {
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
             }
             else
             {
-                if (item.Quality < 50)
+                if (item.SellIn <= 5)
                 {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes for Re:factor"
-                    || item.Name == "Backstage passes for HAXX")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
+                    item.Quality = Clamp(item.Quality + 3);
                 }
-            }
-
-            if (item.Name != "B-DAWG Keychain")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Good Wine")
+                else if (item.SellIn <= 10)
                 {
-                    if (item.Name != "Backstage passes for Re:factor"
-                        && item.Name != "Backstage passes for HAXX")
-                    {
-                        if (item.Name != "B-DAWG Keychain")
-                        {
-                            if (item.Name == "Duplicate Code" ||
-                                item.Name == "Long Methods" ||
-                                item.Name == "Ugly Variable Names")
-                            {
-                                item.Quality = System.Math.Max(0, item.Quality - 2);
-                            }
-                            else
-                            {
-                                item.Quality = System.Math.Max(0, item.Quality - 1);
-                            }
-                        }
-
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
+                    item.Quality = Clamp(item.Quality + 2);
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
+                    item.Quality = Clamp(item.Quality + 1);
                 }
             }
+        }
+
+        static void UpdateQualityForSmellyItems(ref Item item)
+        {
+            item.Quality = Clamp(item.Quality - 2 * Decay(item));
+        }
+
+        static void UpdateQualityForNormalItem(ref Item item)
+        {
+            item.Quality = Clamp(item.Quality - Decay(item));
+        }
+
+        static int Decay(Item item)
+        {
+            return item.SellIn > 0 ? 1 : 2;
+        }
+
+        private static int Clamp(int quality)
+        {
+            if (quality < MinQuality)
+            {
+                return MinQuality;
+            }
+            if (quality > MaxQuality)
+            {
+                return MaxQuality;
+            }
+            return quality;
         }
     }
 }
