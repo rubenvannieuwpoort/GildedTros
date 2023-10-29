9148780a30e9873d9049d0f9d5a97677c96dee89
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-10-29 11:56:34 +0100

    Add some unit tests for normal items

--- CSharp/GildedTros.App/test/UnitTests.cs
+++ CSharp/GildedTros.App/test/UnitTests.cs
@@ -5,13 +5,43 @@ namespace GildedTros.App
 {
     public class UnitTests
     {
-        [Fact]
-        public void foo()
+        public void TestBeforeAfter(Item before, Item after)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            Inventory inventory = new Inventory(Items);
+            List<Item> items = new List<Item> { before };
+            Inventory inventory = new Inventory(items);
+
             inventory.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+
+            Assert.Equal(after.Name, items[0].Name);
+            Assert.Equal(after.SellIn, items[0].SellIn);
+            Assert.Equal(after.Quality, items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityOfNormalItemShouldDecreaseByOneBeforeSellDate()
+        {
+            TestBeforeAfter(
+                new Item { Name = "foo", SellIn = 5, Quality = 10 },
+                new Item { Name = "foo", SellIn = 4, Quality = 9 }
+            );
+        }
+
+        [Fact]
+        public void QualityOfNormalItemShouldDecreaseByTwoAfterSellDate()
+        {
+            TestBeforeAfter(
+                new Item { Name = "foo", SellIn = 0, Quality = 10 },
+                new Item { Name = "foo", SellIn = -1, Quality = 8 }
+            );
+        }
+
+        [Fact]
+        public void QualityOfNormalItemShouldNotBecomeNegative()
+        {
+            TestBeforeAfter(
+                new Item { Name = "foo", SellIn = 0, Quality = 0 },
+                new Item { Name = "foo", SellIn = -1, Quality = 0 }
+            );
         }
     }
 }
